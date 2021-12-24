4f77a8dc0f1660ec07e8af00d348d703b3088393
Sergei Palmer <zigpalmer@gmail.com>
2021-12-24 16:36:31 +0000

    Added useState to feedback item

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
+
 function FeedbackItem() {
+  const [rating, setRating] = useState(7);
+  const [text, setText] = useState('This is a feedback item');
+
   return (
     <div className="card">
-      <div className="num-display">10</div>
-      <div className="text-display">This is a feedback item</div>
+      <div className="num-display">{rating}</div>
+      <div className="text-display">{text}</div>
     </div>
   )
 }
 
-export default FeedbackItem
+export default FeedbackItem;
