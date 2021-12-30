f83c6704f2dca53c8aff8f54a3685e2f7297508e
Sergei Palmer <zigpalmer@gmail.com>
2021-12-30 15:29:22 +0000

    Feedback items and list added with dummy data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem"; 
+import FeedbackData from "./data/FeedbackData"
+import FeedbackList from "./components/FeedbackList";
+import { useState } from 'react';
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackItem />
+        <FeedbackList feedback={feedback}/>
       </div>
     </>
   );
