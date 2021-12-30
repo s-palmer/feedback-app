347206e2f1212b25c280a885f5e651d772e6f3ce
Sergei Palmer <zigpalmer@gmail.com>
2021-12-30 16:28:41 +0000

    Delete function and feedback stats

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData"
 import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
 import { useState } from 'react';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
+  const deleteFeedback = (id) => {
+   if(window.confirm('Are you sure you want to delete this?'))
+   {
+     setFeedback(feedback.filter((item) => item.id !== id))
+   }
+  }
+
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback}/>
+        <FeedbackStats feedback={feedback}/>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
       </div>
     </>
   );
