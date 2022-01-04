651a3f34b8b0fcedf638bc796a62ee6dc4ac4eee
Sergei Palmer <zigpalmer@gmail.com>
2022-01-04 20:22:51 +0000

    Initial form createdf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import Header from "./components/Header";
-import FeedbackData from "./data/FeedbackData"
+import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
-import { useState } from 'react';
+import FeedbackForm from "./components/FeedbackForm";
+import { useState } from "react";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
   const deleteFeedback = (id) => {
-   if(window.confirm('Are you sure you want to delete this?'))
-   {
-     setFeedback(feedback.filter((item) => item.id !== id))
-   }
-  }
+    if (window.confirm("Are you sure you want to delete this?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
 
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackStats feedback={feedback}/>
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+        <FeedbackForm />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
