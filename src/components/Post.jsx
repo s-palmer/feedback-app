138ab71baf621f190717f4d990bf84fc50acc9f2
Sergei Palmer <zigpalmer@gmail.com>
2022-01-05 17:21:45 +0000

    Playing with nested routes and useNavigate

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,26 @@
-
-import { useParams } from 'react-router-dom'
+import { Navigate, useNavigate, Routes, Route } from 'react-router-dom'
 
 function Post() {
-  const params = useParams();
+  const status = 200;
+
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    console.log('Hello')
+    navigate('/post/show')
+  }
+
+  if(status === 404) {
+    return <Navigate to='/not-found' />
+  }
 
   return (
     <div>
-      <h1>Post {params.id}</h1>
-      <h2>{params.name}</h2>
+      <h1>Post</h1>
+      <button onClick={onClick}>Click</button>
+      <Routes>
+        <Route path='/show' element={<h1>Hello world</h1>} />
+      </Routes>
     </div>
   )
 }
