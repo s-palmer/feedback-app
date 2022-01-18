e6cb485819288fa2d027674ccbecf69071d860be
Sergei Palmer <zigpalmer@gmail.com>
2022-01-18 15:43:08 +0000

    posting to json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
@@ -8,9 +7,7 @@ export const FeedbackProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
 
   const fetchFeedback = async () => {
-    const response = await fetch(
-      `http://localhost:5000/feedback?_sort=id&_order=desc`
-    );
+    const response = await fetch("/feedback?_sort=id&_order=desc");
     const data = await response.json();
 
     setFeedback(data);
@@ -39,9 +36,18 @@ export const FeedbackProvider = ({ children }) => {
     });
   };
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch("/feedback", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFeedback),
+    });
+
+    const data = await response.json();
+
+    setFeedback([data, ...feedback]);
   };
 
   const updateFeedback = (id, updItem) => {
